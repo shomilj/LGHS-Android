026ac0b330abdab9d99836c416b6509abc4da053
hairbear <handong.wang@gmail.com>
2017-05-04 13:15:06 -0700

    Added a temporary button to get from launch screen to main activity

--- app/src/main/java/com/sjf/lgcats/LoadingActivity.java
+++ app/src/main/java/com/sjf/lgcats/LoadingActivity.java
@@ -1,18 +1,22 @@
 package com.sjf.lgcats;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class LoadingActivity extends AppCompatActivity {
 
     private TextView orangeBlackDayDisplay;
+    private Button temporaryButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,25 +25,25 @@ public class LoadingActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
         orangeBlackDayDisplay = (TextView) findViewById(R.id.orange_black_day_display);
         String orangeBlackDayDisplayText = "Today " + getString(R.string.orange_day);
         orangeBlackDayDisplay.setText(orangeBlackDayDisplayText);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        temporaryButton = (Button) findViewById(R.id.temporary_button);
+        temporaryButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(LoadingActivity.this, MainActivity.class);
+                startActivity(intent);
             }
         });
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_loading, menu);
+        //getMenuInflater().inflate(R.menu.menu_loading, menu);
         return true;
     }
 
@@ -58,8 +62,17 @@ public class LoadingActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void parse () {
+    public void parse() {
 
+
+    }
+
+    public void parseSchoolCalendar() {
+
+
+    }
+
+    public void parseClubs() {
 
     }
 
