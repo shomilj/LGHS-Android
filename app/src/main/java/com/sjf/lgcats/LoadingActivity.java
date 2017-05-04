75f19391bd3419c5d9be55421da75fe6dc9c7098
hairbear <handong.wang@gmail.com>
2017-05-04 13:55:52 -0700

    Color Changes
    Messed with colors because LG are the orange and black and there’s no
    turning back

--- app/src/main/java/com/sjf/lgcats/LoadingActivity.java
+++ app/src/main/java/com/sjf/lgcats/LoadingActivity.java
@@ -2,19 +2,25 @@ package com.sjf.lgcats;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class LoadingActivity extends AppCompatActivity {
 
+    private TextView todaysDate;
     private TextView orangeBlackDayDisplay;
     private Button temporaryButton;
 
@@ -25,9 +31,23 @@ public class LoadingActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Date date = new Date();
+        //Calendar cal = Calendar.getInstance();
+
+        todaysDate = (TextView) findViewById(R.id.todays_date);
+        todaysDate.setText(dateFormat.format(date));
+
         orangeBlackDayDisplay = (TextView) findViewById(R.id.orange_black_day_display);
         String orangeBlackDayDisplayText = "Today " + getString(R.string.orange_day);
         orangeBlackDayDisplay.setText(orangeBlackDayDisplayText);
+
+        parse();
+
+        Window window = this.getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.setStatusBarColor(ContextCompat.getColor(this, R.color.black));
 
         temporaryButton = (Button) findViewById(R.id.temporary_button);
         temporaryButton.setOnClickListener(new View.OnClickListener() {
@@ -63,8 +83,8 @@ public class LoadingActivity extends AppCompatActivity {
     }
 
     public void parse() {
-
-
+        parseSchoolCalendar();
+        parseClubs();
     }
 
     public void parseSchoolCalendar() {
--- app/src/main/java/com/sjf/lgcats/MainActivity.java
+++ app/src/main/java/com/sjf/lgcats/MainActivity.java
@@ -1,11 +1,11 @@
 package com.sjf.lgcats;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,14 +16,11 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        Window window = this.getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.setStatusBarColor(ContextCompat.getColor(this, R.color.black));
+
     }
 
 }
