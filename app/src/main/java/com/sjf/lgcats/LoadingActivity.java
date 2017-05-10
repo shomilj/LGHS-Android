632b899f3a12f1d2bea042354ad17573888ad943
hairbear <handong.wang@gmail.com>
2017-05-09 20:25:51 -0700

    Began writing file-downloading code
    Added some commented code snippets that are meant to download the
    spreadsheet
    
    Also just made colorPrimaryDark pure black to simplify life

--- app/src/main/java/com/sjf/lgcats/LoadingActivity.java
+++ app/src/main/java/com/sjf/lgcats/LoadingActivity.java
@@ -1,7 +1,12 @@
 package com.sjf.lgcats;
 
+import android.app.DownloadManager;
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -13,8 +18,10 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.io.File;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 public class LoadingActivity extends AppCompatActivity {
@@ -25,28 +32,28 @@ public class LoadingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // get the date
         DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
         Date date = new Date();
         //Calendar cal = Calendar.getInstance();
 
+        // display today's date
         todaysDate = (TextView) findViewById(R.id.todays_date);
         todaysDate.setText(dateFormat.format(date));
 
+        // display whether today is a black day or an orange day
         orangeBlackDayDisplay = (TextView) findViewById(R.id.orange_black_day_display);
         String orangeBlackDayDisplayText = "Today " + getString(R.string.orange_day);
         orangeBlackDayDisplay.setText(orangeBlackDayDisplayText);
 
+        // parse necessary info upon loading
         parse();
-
-        Window window = this.getWindow();
-        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-        window.setStatusBarColor(ContextCompat.getColor(this, R.color.black));
 
         temporaryButton = (Button) findViewById(R.id.temporary_button);
         temporaryButton.setOnClickListener(new View.OnClickListener() {
@@ -85,8 +92,25 @@ public class LoadingActivity extends AppCompatActivity {
     }
 
     public void parse() {
+
+        /*File dir = new File(Environment.getExternalStorageDirectory() + "/Download/your folder/");
+        dir.mkdirs();
+
+        String url = "bit.ly/LGCATSlinks";
+        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+        request.setDescription("Some description");
+        request.setTitle("Some title");
+        request.allowScanningByMediaScanner();
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "name-of-the-file.ext");
+
+        // get download service and enqueue file
+        DownloadManager manager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+        manager.enqueue(request);
+
+        ArrayList<ArrayList<String>> s = StringUtil.parseTSV();
         parseSchoolCalendar();
-        parseClubs();
+        parseClubs();*/
     }
 
     public void parseSchoolCalendar() {
@@ -97,5 +121,4 @@ public class LoadingActivity extends AppCompatActivity {
     public void parseClubs() {
 
     }
-
 }
--- app/src/main/java/com/sjf/lgcats/MainActivity.java
+++ app/src/main/java/com/sjf/lgcats/MainActivity.java
@@ -6,21 +6,35 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
+
+// Method header
+/**
+ *
+ */
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView dayTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("LG CATS");
 
-        Window window = this.getWindow();
-        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-        window.setStatusBarColor(ContextCompat.getColor(this, R.color.black));
+        // display whether it is a black or an orange day
+        dayTextView = (TextView) findViewById(R.id.day_text_view);
+        dayTextView.setText(R.string.black_day);
+
+        // change action bar to black
+        //Window window = this.getWindow();
+        //window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        //window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        //window.setStatusBarColor(ContextCompat.getColor(this, R.color.black));
 
     }
 
