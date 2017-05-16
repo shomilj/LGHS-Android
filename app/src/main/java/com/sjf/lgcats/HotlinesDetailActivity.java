fe8adfd29952f9fd3bb3ad1f382acbb6ab332b4c
shomilj <shomil.edu@gmail.com>
2017-05-16 10:17:45 -0700

    hotline classes added

---
+++ app/src/main/java/com/sjf/lgcats/HotlinesDetailActivity.java
@@ -0,0 +1,15 @@
+package com.sjf.lgcats;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HotlinesDetailActivity extends AppCompatActivity {
+
+    private Hotline hotline;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hotlines_detail);
+    }
+}
