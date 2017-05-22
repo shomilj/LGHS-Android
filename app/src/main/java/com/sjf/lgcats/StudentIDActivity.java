45ba84db6de54bbe31c15e52f597af65de1b4ed2
hairbear <handong.wang@gmail.com>
2017-05-22 09:45:50 -0700

    cite the barcode
    b/c they deserve it

--- app/src/main/java/com/sjf/lgcats/StudentIDActivity.java
+++ app/src/main/java/com/sjf/lgcats/StudentIDActivity.java
@@ -1,14 +1,17 @@
 package com.sjf.lgcats;
 
-import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.widget.EditText;
 import android.widget.TextView;
 
 /**
+ *
+ * Displays a barcode for the student's student ID.
+ * barcode software font came from here:
+ * https://www.barcodesinc.com/free-barcode-font/
+ *
  * @author  Shomil Jain
  * @author  Quintin Leary
  * @author  Cassandra Melax
